f8f0bfad33b023cdc17d7d3ae2b45aedb907e5f3
sebastian.garcia70 <sebastiangz3108@gmail.com>
2022-04-28 16:09:16 -0500

    fghtht

---
+++ SGPI/Models/ProgramaUsuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SGPI.Models
+{
+    public partial class ProgramaUsuario
+    {
+        public int IdProgramaUsu { get; set; }
+        public int? IdUsuario { get; set; }
+        public int? IdPrograma { get; set; }
+
+        public virtual Usuario IdPrograma1 { get; set; }
+        public virtual Programa IdProgramaNavigation { get; set; }
+    }
+}
