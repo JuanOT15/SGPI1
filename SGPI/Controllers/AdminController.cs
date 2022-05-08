08e8e35510c5a3e42f9f841e2ad6d8c168825702
JuanOT15 <99086881+JuanOT15@users.noreply.github.com>
2022-05-08 13:04:09 -0500

    PT2 intefraz

--- SGPI/Controllers/AdminController.cs
+++ SGPI/Controllers/AdminController.cs
@@ -8,88 +8,94 @@ namespace SGPI.Controllers
     {
         private SGPI_DBContext context;
 
+
+
         public AdminController(SGPI_DBContext contexto)
         {
             context = contexto;
         }
+
         public IActionResult AdministrarUsuario()
         {
-            return View();
+            Usuario us = new Usuario();
+            return View(us);
         }
-        [HttpPost]
-        public IActionResult AdministrarUsuario(Usuario user)
-        {
-            var listaUsuario = context.Usuarios
-                .Where(u => u.Documento.Contains(user.Documento)).ToList();
-            return View(listaUsuario);
-        }
-
 
         public IActionResult CrearUsuario()
         {
             ViewBag.programa = context.Programas.ToList();
-            ViewBag.tipoDoc = context.TipoDocumentos.ToList();
             ViewBag.genero = context.Generos.ToList();
             ViewBag.rol = context.Rols.ToList();
+            ViewBag.tipoDoc = context.TipoDocumentos.ToList();
+
             return View();
         }
+
         [HttpPost]
         public IActionResult CrearUsuario(Usuario usuario)
         {
-            ViewBag.programa = context.Programas.ToList();
-            ViewBag.tipoDoc = context.TipoDocumentos.ToList();
-            ViewBag.genero = context.Generos.ToList();
-            ViewBag.rol = context.Rols.ToList();
             context.Add(usuario);
             context.SaveChanges();
+            ViewBag.genero = context.Generos.ToList();
+            ViewBag.rol = context.Rols.ToList();
+            ViewBag.programa = context.Programas.ToList();
+            ViewBag.tipoDoc = context.TipoDocumentos.ToList();
+
             return View();
         }
 
 
-        public IActionResult MenuAdmin()
-        {
-            return View();
-        }
 
-        /*
-        public ActionResult BuscarUsuario(Usuario user)
+
+        [HttpPost]
+        public IActionResult AdministrarUsuario(Usuario usuario)
         {
-            Usuario usuario = context.Usuarios.Find(user.IdUsuario);
-            if (usuario == null)
+            var buscarUsuario = context.Usuarios.Where(u => u.Documento.Contains(usuario.Documento));
+
+            if (buscarUsuario != null)
             {
-                return ViewBag.mensaje = "Error al eliminar el usuario, El usuario no existe";
-            }
-            user.Nombre = "";
-            
-
-            context.Usuarios.Update(user);
-            return View();
-        }
-
-        public ActionResult Edit(Usuario user)
-        {
-            Usuario usuario = context.Usuarios.Find(user.IdUsuario);
-                if (usuario == null)
-            {
-                return ViewBag.mensaje = "Error al eliminar el usuario, El usuario no existe";
+                return View(buscarUsuario.FirstOrDefault());
             }
             return View();
         }
 
-        public IActionResult Delete(Usuario user)
+        public IActionResult Admin()
         {
-            Usuario usuario = context.Usuarios.Find(user.IdUsuario);
-                if (usuario == null)
+            return View();
+        }
+
+        public ActionResult Edit(Usuario usuario)
+        {
+
+            Usuario user = context.Usuarios.Find(usuario.IdUsuario);
+
+            if (user == null)
             {
-                return ViewBag.mensaje = "Error al eliminar el usuario, El usuario no existe";
+                return ViewBag.mensaje = "Error al editar el usuaiario";
+            }
+
+            usuario.Nombre = "";
+            usuario.Apellido = "";
+
+            context.Usuarios.Update(usuario);
+
+            return View();
+        }
+
+        public ActionResult Delete(Usuario usuario)
+        {
+            Usuario user = context.Usuarios.Find(usuario.IdUsuario);
+
+            if (user == null)
+            {
+                return ViewBag.mensaje = "Error al eliminar el usuario";
             }
             else
             {
                 context.Remove(user);
                 context.SaveChanges();
             }
-            return View();
+            return RedirectToAction("AdministrarUsuario");
         }
-        */
     }
 }
