39e617f755965384dd463330541c010b7ddd9771
sebastian.garcia70 <sebastiangz3108@gmail.com>
2022-05-08 23:34:32 -0500

    ffj

--- SGPI/Startup.cs
+++ SGPI/Startup.cs
@@ -24,7 +24,7 @@ namespace SGPI
 
             string connectionString = ConfigurationExtensions
                 .GetConnectionString(this.Configuration,
-                "DefaultConnectionStringOTA");
+                "DefaultConnectionString");
             services.AddDbContext<SGPI_DBContext>(
                 options => options.UseSqlServer(connectionString)
                 );
