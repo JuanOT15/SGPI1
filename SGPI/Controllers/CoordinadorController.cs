5d15c2b19354c80ac6b8184450466545eed18c6d
sebastian.garcia70 <sebastiangz3108@gmail.com>
2022-05-16 22:16:38 -0500

    fg

--- SGPI/Controllers/CoordinadorController.cs
+++ SGPI/Controllers/CoordinadorController.cs
@@ -1,16 +1,53 @@
 using Microsoft.AspNetCore.Mvc;
-
+using SGPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 namespace SGPI.Controllers
 {
     public class CoordinadorController : Controller
     {
+        private SGPI_DBContext context;
+
+        public CoordinadorController(SGPI_DBContext contexto)
+        {
+            context = contexto;
+        }
+
         public IActionResult Consultas()
         {
+            Usuario us = new Usuario();
+            return View(us);
+        }
+
+        [HttpPost]
+        public IActionResult Consultas(Usuario usuario)
+        {
+            var buscarUsuario = context.Usuarios.Where(u => u.Documento.Contains(usuario.Documento) && u.Rol == 3);
+
+            if (buscarUsuario != null)
+            {
+                return View(buscarUsuario.FirstOrDefault());
+            }
             return View();
         }
 
         public IActionResult EntrevistaAdmision()
         {
+            Usuario us = new Usuario();
+            return View(us);
+        }
+
+        [HttpPost]
+        public IActionResult EntrevistaAdmision(Usuario usuario)
+        {
+            var buscarUsuario = context.Usuarios.Where(u => u.Documento.Contains(usuario.Documento));
+
+            if (buscarUsuario != null)
+            {
+                return View(buscarUsuario.FirstOrDefault());
+            }
             return View();
         }
         public IActionResult HomologacionAsignatura()
